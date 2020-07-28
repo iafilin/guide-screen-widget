d30f6d06ab1eb4e8779e052167c2f32ce370787a
Игорь Александрович <geekfil@gmail.com>
2020-07-28 14:39:00 +0300

    rebuild

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faArrowLeft, fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas)
+library.add(faArrowRight);
+library.add(faArrowLeft);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
